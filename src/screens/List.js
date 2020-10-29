75dfba9871273fb6ba04114b58487657b189a744
Anny Confessor <annyconfessor.ectufrn@gmail.com>
2020-10-29 17:09:11 -0300

    listPokemons

--- src/screens/List.js
+++ src/screens/List.js
@@ -2,21 +2,20 @@ import React, { useEffect, useState } from 'react';
 import { listPokemons } from '../services/pokeapi'
 
 const List = () => {
-    const [pokes, setPokes] = useState({})
+    const [pokes, setPokes] = useState([])
 
     useEffect( async () => {
         const pokes = await listPokemons() //a list pokemon n precisa de params
-        console.log(pokes.results[0].url)
 
-        setPokes(pokes)
+        setPokes(pokes.results)
     }, [])
 
+    console.log(pokes)
+
     return (
-        <button className="poke-name">
-            {console.log(pokes.results)}
-            
-            {/* {pokes.map(pokes => <div>{pokes}</div>)} */}
-        </button>
+        <div className="poke-name">
+            {pokes.map(item => <button>{item.name}</button>)}
+        </div>
     )
 }
 
